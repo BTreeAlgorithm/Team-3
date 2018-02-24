7a44774fd72b05f05297f0e16fbf2e7ade41cd36
FrankXuCN <yxu2017@my.fit.edu>
2018-02-24 09:50:24 -0500

    project architecture

--- unistd.h
+++ unistd.h
@@ -1,9 +1,13 @@
+// function return value
 #define FALSE        1
 #define TRUE         0
 
+// Error Code
 #define E_NOERR      0
 #define E_ARGV       1
 #define E_GET_B_TREE 2
+
+int ErrNum;  // A globle variable represent all kinds of errors.
 
 struct B_Tree
 {
@@ -18,8 +22,6 @@ struct B_Tree
     // to use this variable to judge if it is necessary to seperate.
     int key_counter;   
 };
-
-int ErrNum;  // A globle variable represent all kinds of errors.
 
 int insert(B_Tree * p_tree, int value);
 int search(B_Tree * p_tree, int value);
