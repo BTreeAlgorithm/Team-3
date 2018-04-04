9f215237ca9797bb252996792aecf158cbf3e247
FrankXuCN <yxu2017@my.fit.edu>
2018-04-04 19:51:20 -0400

    add compiler command

--- autotest.sh
+++ autotest.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # the executable file btree and random should be in the same direction.
+gcc btree.c -Wall -pg -o btree
+gcc random.c -Wall -o random
 echo Choose what you want to implement: insert--i, delete--d, or seach--s &&
 read func &&
 if [[ $(find profiling -type d) ]]; then
 	rm -r profiling
 fi
 mkdir profiling
-for i in {10..10..10}; do
+for i in {10..1000..10}; do
     #statements
     ./random $i
     ./btree $func $RANDOM
